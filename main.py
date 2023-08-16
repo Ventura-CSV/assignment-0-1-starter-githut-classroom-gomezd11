b1fcdcb1163205d8410639b17a027da7e8633ed1
gomezd11 <gomezd1290@gmail.com>
2023-08-16 16:47:35 -0700

    Assigment 1

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     # print('Hello World')
     ##############################
 
+    print("Hello, World")
+    print("CS7: Python Programming")
+
 
 if __name__ == '__main__':
     main()
